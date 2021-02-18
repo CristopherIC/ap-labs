5be9841c3b4dd23753776bc441d722cb61768f8a
Cristopher Roman Ibarra Claudio <A01611408@itesm.mx>
2021-02-18 00:48:21 +0000

    Solved
    Signed-off-by: Cristopher Roman Ibarra Claudio <A01611408@itesm.mx>

--- labs/c-basics/fahrenheit_celsius.c
+++ labs/c-basics/fahrenheit_celsius.c
@@ -1,13 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, int **argv)
+
+int main(int argc, char **argv)
 {
-    if(argc > 2){
-        printf("Just 1 value\n");
-    } else {
-        int fahr = argv[0];
-        printf("%d\n", argv[0]);
+    int fahr;
+
+    if (argc == 4) {
+        int totalSteps = (atoi(argv[2]) - atoi(argv[1])) / atoi(argv[3]);
+        
+        fahr = atoi(argv[1]);
+        printf("Fahrenheit: %3d, Celcius: %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+
+        for(int i = 0; i < totalSteps; i++){
+            fahr = fahr + atoi(argv[3]);
+            printf("Fahrenheit: %3d, Celcius: %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+        }
+    } else if (argc == 2){
+        fahr = atoi(argv[1]);
 	    printf("Fahrenheit: %3d, Celcius: %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
-    }
+    } else {
+        printf("Invalid Input\n");
+    } 
+
     return 0;
 }
+
+//Bibliography
+//https://cboard.cprogramming.com/c-programming/134232-converting-command-line-char-*argv[]-int.html 
