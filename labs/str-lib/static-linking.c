1e4246f4820c7cf1b72fd23d6f9eddd8f4f63189
Cristopher Roman Ibarra Claudio <A01611408@itesm.mx>
2021-03-24 03:34:48 +0000

    Solved str-lib
    Signed-off-by: Cristopher Roman Ibarra Claudio <A01611408@itesm.mx>

--- labs/str-lib/static-linking.c
+++ labs/str-lib/static-linking.c
@@ -1,5 +1,36 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
-    return 0;
+int mystrlen(char *str);
+char *mystradd(char *origin, char *addition);
+int mystrfind(char *origin, char *substr);
+
+int main(int argc,char **argv) {
+    
+    if(argc < 4){
+        printf("Not enouht arguments\n");
+    } else {
+        int size1 = mystrlen(argv[2]);
+        int size2 = mystrlen(argv[3]);
+
+        if(strcmp(argv[1], "-add") == 0) {
+            printf("Initial length\t: %d\n",mystrlen(argv[2]));
+            char *outcome = mystradd(argv[2], argv[3]);
+            printf("New String\t: %s\n" , outcome);
+            printf("New length\t: %d\n",mystrlen(outcome));
+
+        } else if(strcmp(argv[1], "-find") == 0){
+            int pos = mystrfind(argv[2],argv[3]);
+            if(pos < 0){
+                printf("['%s'] is not in the first string\n", argv[3]);
+            } else {
+                printf("['%s'] was found at [%d] position\n", argv[3], pos);
+            }
+        } else {
+            printf("Invalid parameters\n");
+        }
+        return 0;
+    }
+    
+        
 }
--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,54 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int mystrlen(char *str){
-    return 0;
+    int size = 0;
+
+    while(*str != '\0'){
+        *str++;
+        size++;
+    }
+
+    return size;
 }
 
 char *mystradd(char *origin, char *addition){
-    return 0;
+    
+    int originS = mystrlen(origin);
+    int additionS = mystrlen(addition);
+    char *outcome = malloc(originS + additionS);
+
+    for(int i = 0; i < originS; i++){
+        outcome[i] = origin[i];
+    }
+
+    int aux = 0;
+    for(int j = originS; j < (originS + additionS); j++){
+        outcome[j] = addition[aux];
+        aux++;
+    }
+
+    return outcome;
 }
 
 int mystrfind(char *origin, char *substr){
-    return 0;
+    
+    int originS = mystrlen(origin);
+    int subS = mystrlen(substr);
+
+     for(int i = 0; i < originS; i++){
+        if(origin[i] == substr[0]){
+            for(int j = 0; j < subS; j++){
+                if(origin[i+j] != substr[j]){
+                    // Keep searching 
+                    break;
+                } else if(j == subS-1){
+                    //We only reach this condition after validating the entire string
+                    return i;
+                }
+            }
+        }
+    }
+    return -1;
 }
